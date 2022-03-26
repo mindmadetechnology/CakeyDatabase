8e49280f634e1f55c4c73577a2829e7c2595fd60
admin <karthickdurai587@gmail.com>
2022-03-26 17:01:09 +0530

    all

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const db = require('./config/db');
 const app = express();
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+  
+// Set EJS as templating engine 
+app.set("view engine", "ejs");
 const authRoute = require("./routes/auth");
 
 app.use('/api',authRoute);
