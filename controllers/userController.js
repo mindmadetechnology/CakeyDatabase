ce4d8d3bb917f6f0ab7e037b472b67aa677ddb22
admin <karthickdurai587@gmail.com>
2022-03-30 17:00:56 +0530

    all

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,7 +27,7 @@ const putUsers = (req, res) => {
     const userId = req.params.userId;
     const Modified_On = moment().format("DD-MM-YYYY, h:mm a")
 
-    res.send({ statusCode: 200, message: file,name:UserName,Address:Address })
+    res.send({ statusCode: 200, name:UserName,Address:Address })
     // userModel.findById({ _id: userId }, function (err, result) {
     //     if (err) {
     //         res.send(err);
