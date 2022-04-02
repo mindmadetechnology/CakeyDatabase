e5d86fb4297288f2bc6b950e4793347b942ea8c4
muthu <muthu@sengen.com>
2022-04-02 13:47:24 +0530

    all

--- models/CakeModels.js
+++ models/CakeModels.js
@@ -10,9 +10,9 @@ const CakeSchema = new mongoose.Schema({
     TypeOfCake: {
         type: String
     },
-    Images: [{
-        type: String
-    }],
+    Images: [
+
+    ],
 
     eggOrEggless: {
         type: String
@@ -38,62 +38,10 @@ const CakeSchema = new mongoose.Schema({
     Modified_On: {
         type: String
     },
-    FlavorList: {
-        flavor1: {
-            type: String
-        },
-        flavor2: {
-            type: String
-        },
-        flavor3: {
-            type: String
-        },
-        flavor4: {
-            type: String
-        }
-    },
-    ShapesLists: {
-        Shape1: {
-            type: String
-        },
-        Shape2: {
-            type: String
-        },
-        Shape3: {
-            type: String
-        },
-        Shape4: {
-            type: String
-        }
-    },
-    CakeToppings: {
-        Toppings1: {
-            type: String
-        },
-        Toppings2: {
-            type: String
-        },
-        Toppings3: {
-            type: String
-        },
-        Toppings4: {
-            type: String
-        }
-    },
-    WeightList: {
-        Weigth1: {
-            type: String
-        },
-        Weigth2: {
-            type: String
-        },
-        Weigth3: {
-            type: String
-        },
-        Weigth4: {
-            type: String
-        }
-    },
+    FlavorList: [],
+    ShapesLists: [],
+    CakeToppings: [],
+    WeightList: [],
     IsDeleted: {
         type: String,
         default: 'n'
