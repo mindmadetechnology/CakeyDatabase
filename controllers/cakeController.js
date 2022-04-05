ec2cfa52d0b626620bad58cd4388a162555b4387
admin <karthickdurai587@gmail.com>
2022-04-05 10:29:14 +0530

    all

--- controllers/cakeController.js
+++ controllers/cakeController.js
@@ -19,7 +19,7 @@ const getcakelist = (req, res) => {
 // get cake list
 const getcakelistByVendorName = (req, res) => {
     const VendorName = req.params.VendorName;
-    cakeModel.find({ VendorName : VendorName , IsDeleted: 'n' }, function (err, result) {
+    cakeModel.find({ VendorName: VendorName, IsDeleted: 'n' }, function (err, result) {
         if (err) {
             res.send({ statusCode: 400, message: "There  is was a problem adding the information to the database." });
         } else {
@@ -86,7 +86,7 @@ const addCake = async (req, res) => {
                 ShapeList: ShapeList,
                 CakeToppings: CakeToppings,
                 WeightList: WeightList,
-                Stock : Stock,
+                Stock: Stock,
                 Created_On: Created_On
 
             });
@@ -121,15 +121,15 @@ const updateCake = (req, res) => {
     const CakeToppings = req.body.CakeToppings;
     const WeightList = req.body.WeightList;
     const Stock = req.body.Stock;
-    const Modified_On = moment().tz('Asia/Kolkata').format("DD-MM-YYYY hh:mm A");                 
+    const Modified_On = moment().tz('Asia/Kolkata').format("DD-MM-YYYY hh:mm A");
 
     try {
-        cakeModel.findById({ _id: id },async function (err, result) {
+        cakeModel.findById({ _id: id }, async function (err, result) {
             if (err) {
                 res.send({ statusCode: 400, message: "Failed" })
             } else if (result === null) {
                 res.send({ statusCode: 400, message: "Failed" })
-            }else {
+            } else {
                 if (req.files === undefined || req.files === null) {
                     cakeModel.findOneAndUpdate({ _id: id },
                         {
@@ -148,7 +148,7 @@ const updateCake = (req, res) => {
                                 ShapeList: ShapeList,
                                 CakeToppings: CakeToppings,
                                 WeightList: WeightList,
-                                Stock : Stock,
+                                Stock: Stock,
                                 Modified_On: Modified_On,
 
                             }
@@ -159,87 +159,45 @@ const updateCake = (req, res) => {
                                 res.send({ statusCode: 200, message: "Updated Successfully" });
                             }
                         });
-                } else if(req.files && imageUrl !== null){
+                } else {
 
-                    var imageUrlList = [...imageUrl];
+                    var imageUrlList = imageUrl;
 
                     for (let i = 0; i < req.files.length; i++) {
-                      await cloudinary.uploader.upload(req.files[i].path,function(err,result){
+                        await cloudinary.uploader.upload(req.files[i].path, function (err, result) {
                             imageUrlList.push(result.url);
-                        })         
+                        })
                     }
-                    
-                    if (imageUrlList !== []) {
-                        cakeModel.findOneAndUpdate({ _id: id },
-                            {
-                                $set: {
-                                    Title: Title,
-                                    Description: Description,
-                                    TypeOfCake: TypeOfCake,
-                                    Images: imageUrlList,
-                                    EggOrEggless: EggOrEggless,
-                                    Price: Price,
-                                    Ratings: Ratings,
-                                    VendorID: VendorID,
-                                    VendorName: VendorName,
-                                    VendorPhoneNumber: VendorPhoneNumber,
-                                    FlavourList: FlavourList,
-                                    ShapeList: ShapeList,
-                                    CakeToppings: CakeToppings,
-                                    WeightList: WeightList,
-                                    Stock : Stock,
-                                    Modified_On: Modified_On,
+                    cakeModel.findOneAndUpdate({ _id: id },
+                        {
+                            $set: {
+                                Title: Title,
+                                Description: Description,
+                                TypeOfCake: TypeOfCake,
+                                Images: imageUrlList,
+                                EggOrEggless: EggOrEggless,
+                                Price: Price,
+                                Ratings: Ratings,
+                                VendorID: VendorID,
+                                VendorName: VendorName,
+                                VendorPhoneNumber: VendorPhoneNumber,
+                                FlavourList: FlavourList,
+                                ShapeList: ShapeList,
+                                CakeToppings: CakeToppings,
+                                WeightList: WeightList,
+                                Stock: Stock,
+                                Modified_On: Modified_On,
 
-                                }
-                            }, function (err, result) {
-                                if (err) {
-                                    res.send({ statusCode: 400, message: "Failed" });
-                                } else {
-                                    res.send({ statusCode: 200, message: "Updated Successfully" });
-                                }
-                            });
-                    }
-                }else {
-                    var imageUrlList = [];
-
-                    for (let i = 0; i < req.files.length; i++) {
-
-                        // Upload the local image to Cloudinary
-                        // and get image url as response
-                        var result = await cloudinary.uploader.upload(req.files[i].path);
-                        imageUrlList.push(result.url);
-                    }
-                    if (imageUrlList !== []) {
-                        cakeModel.findOneAndUpdate({ _id: id },
-                            {
-                                $set: {
-                                    Title: Title,
-                                    Description: Description,
-                                    TypeOfCake: TypeOfCake,
-                                    Images: imageUrlList,
-                                    EggOrEggless: EggOrEggless,
-                                    Price: Price,
-                                    Ratings: Ratings,
-                                    VendorID: VendorID,
-                                    VendorName: VendorName,
-                                    VendorPhoneNumber: VendorPhoneNumber,
-                                    FlavourList: FlavourList,
-                                    ShapeList: ShapeList,
-                                    CakeToppings: CakeToppings,
-                                    WeightList: WeightList,
-                                    Stock : Stock,
-                                    Modified_On: Modified_On,
-
-                                }
-                            }, function (err, result) {
-                                if (err) {
-                                    res.send({ statusCode: 400, message: "Failed" });
-                                } else {
-                                    res.send({ statusCode: 200, message: "Updated Successfully" });
-                                }
-                            });
-                    }
+                            }
+                        }, function (err, result) {
+                            if (err) {
+                                res.send({ statusCode: 400, message: "Failed" });
+                            } else {
+                                res.send({ statusCode: 200, message: "Updated Successfully" });
+                            }
+                        });
                 }
+
             }
         })
     } catch (err) {
