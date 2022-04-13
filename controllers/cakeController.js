842a8d8a0c2a58d4c957550bb4f356a4053768b7
madhupriya2317 <madhupriya@mindmade.in>
2022-04-13 12:59:03 +0530

    all

--- controllers/cakeController.js
+++ controllers/cakeController.js
@@ -172,35 +172,7 @@ const updateCake = (req, res) => {
             } else if (result === null) {
                 res.send({ statusCode: 400, message: "Failed2" });
             } else {
-                if (req.files === undefined || req.files === null) {
-                    cakeModel.findOneAndUpdate({ _id: id },
-                        {
-                            $set: {
-                                Title: Title,
-                                Description: Description,
-                                TypeOfCake: TypeOfCake,
-                                Images: Images,
-                                EggOrEggless: EggOrEggless,
-                                Price: Price,
-                                Discount: Discount,
-                                Ratings: Ratings,
-                                FlavourList: FlavourList,
-                                ShapeList: ShapeList,
-                                CakeToppings: CakeToppings,
-                                WeightList: WeightList,
-                                Stock: Stock,
-                                Modified_On: Modified_On,
-
-                            }
-                        }, function (err, result) {
-                            if (err) {
-                                res.send({ statusCode: 400, message: "Failed3" });
-                            } else {
-                                res.send({ statusCode: 200, message: "Updated Successfully" });
-                            }
-                        });
-                } else {
-
+                if (req.files.length > 0) {
                     // res.send({ statusCode: 200, message: "rgtrsyrts" });
                     if (Images === null || Images === undefined || Images === []) {
                         var imageUrlList = [];
@@ -235,6 +207,34 @@ const updateCake = (req, res) => {
                         }, function (err, result) {
                             if (err) {
                                 res.send({ statusCode: 400, message: "Failed4" });
+                            } else {
+                                res.send({ statusCode: 200, message: "Updated Successfully" });
+                            }
+                    });
+                   
+                } else {
+                    cakeModel.findOneAndUpdate({ _id: id },
+                        {
+                            $set: {
+                                Title: Title,
+                                Description: Description,
+                                TypeOfCake: TypeOfCake,
+                                Images: Images,
+                                EggOrEggless: EggOrEggless,
+                                Price: Price,
+                                Discount: Discount,
+                                Ratings: Ratings,
+                                FlavourList: FlavourList,
+                                ShapeList: ShapeList,
+                                CakeToppings: CakeToppings,
+                                WeightList: WeightList,
+                                Stock: Stock,
+                                Modified_On: Modified_On,
+
+                            }
+                        }, function (err, result) {
+                            if (err) {
+                                res.send({ statusCode: 400, message: "Failed3" });
                             } else {
                                 res.send({ statusCode: 200, message: "Updated Successfully" });
                             }
