7bd1e20e2f455f5b1c200a76738da967b5dd2825
madhupriya2317 <madhupriya@mindmade.in>
2022-05-06 11:33:00 +0530

    All

--- routes/auth.js
+++ routes/auth.js
@@ -133,7 +133,7 @@ router.get("/vendors/list", getVendors);
 router.get("/vendors/list/:Status", GetNewVendorList);
 
 //Get vendor's details based on email
-router.get("/vendors/list/:email", getVendorsbyEmail);
+router.get("/vendors/listbyemail/:email", getVendorsbyEmail);
 
 //Register vendor
 router.post("/vendors/register",upload.single("file"), RegisterVendors);
