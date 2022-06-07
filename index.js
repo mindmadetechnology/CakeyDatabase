1c98e8455b3794af1462370bf351629a2628b62d
Madhupriya2317 <madhupriya@mindmade.in>
2022-06-07 15:56:00 +0530

    all

--- index.js
+++ index.js
@@ -12,16 +12,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const authRoute = require("./routes/auth");
-const { UpdateOrderResponse } = require('./controllers/orderListController');
-// const OrdersListModel = require('./models/OrdersListModels');
-// const moment = require('moment-timezone');
+const OrdersListModel = require('./models/OrdersListModels');
+const moment = require('moment-timezone');
 
 app.use('/api', authRoute);
 
-app.use(function(req,res) {
-  UpdateOrderResponse();
-});
-
+setInterval((req, res) => {
+  OrdersListModel.find({ Vendor_Response_Status: 'unseen' }, function (err, result) {
+    if (!err) {
+      if (result !== null) {
+        result.map((val) => {
+          var today = moment(new Date()).format("DD-MM-YYYY hh:mm A");
+          const ms = moment(today, "DD-MM-YYYY HH:mm A").diff(moment(val.Created_On, "DD-MM-YYYY HH:mm A"));
+          var d = moment.duration(ms);
+          var s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
+          var a = s.split(':');
+          var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+          if (seconds === 300 || seconds > 300) {
+            OrdersListModel.findOneAndUpdate({ _id: val._id }, {
+              $set: {
+                Vendor_Response_Status: 'no response'
+              }
+            }, function (err, result) {
+              if (err) {
+                res.send({ statusCode: 400, message: 'Failed' });
+              } else {
+                res.send({ statusCode: 200, message: 'Updated Successfully' });
+                console.log("updated");
+              }
+            });
+          }
+        });
+      } else {
+        return null;
+      }
+    }
+  });
+}, 5000);
 // const job = schedule.scheduleJob('* * * * * *', function (req, res) {
 //   OrdersListModel.find({ Vendor_Response_Status: 'unseen' }, function (err, result) {
 //     if (!err) {
