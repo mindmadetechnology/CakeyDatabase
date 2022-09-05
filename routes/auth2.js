d7912140b09d4707b63123a1bbd14c081955859f
Madhupriya2317 <madhupriya@mindmade.in>
2022-09-05 13:01:03 +0530

    Changes

--- routes/auth2.js
+++ routes/auth2.js
@@ -53,7 +53,7 @@ router.delete('/admin/removenotification/:id', RemoveAdminNotification);
 
 router.post('/hamper/new', upload.fields([{name: 'HamperImage', maxCount: 1},{name:'AdditionalHamperImage', maxCount: 5}]), CreateHampers);
 
-router.put('/hamper/update/:id', upload.single('file'), UpdateHampers);
+router.put('/hamper/update/:id', upload.fields([{name: 'HamperImage', maxCount: 1},{name:'AdditionalHamperImage', maxCount: 5}]), UpdateHampers);
 
 router.get('/hamper/list', Authorization, GetHampersList);
 
